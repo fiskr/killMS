bf28b15c8786a7fbdd4189305cb621d3fa2f95f0
U-SN\905270 <905270@snkxw-bfoster3.sn.corp>
2014-03-21 12:57:50 -0400

    Initializing killMS project

---
+++ killMS.js
@@ -0,0 +1,13 @@
+/*
+ * Author: Brandon Foster
+ * Date: March 21, 2014
+ * Purpose: Kill Microsoft Word Document special characters that don't render properly in HTML pages
+ */
+ 
+ var 
+ dog,
+ bait,
+ result = null;
+ 
+ 
+		
